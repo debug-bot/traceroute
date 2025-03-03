3400b276f6daea960926d5a9cf445c304d21e502
Adnan Ashraf <adnanashraf4423@gmail.com>
2025-03-04 00:52:06 +0500

    fixed

--- app/views.py
+++ app/views.py
@@ -476,7 +476,10 @@ def download_configuration(request):
     for dev in selected_devices:
         device_id = dev["id"]
         device_ip = dev["ip"]
-        output = execute_ssh_command('show configuration | display set', hostname=device_ip, delay_in_seconds=5)
+        try:
+            output = execute_ssh_command('show configuration | display set', hostname=device_ip, delay_in_seconds=5)
+        except:
+            output = '0'
         configuration_data[device_id] = output
 
     return JsonResponse({"configuration": configuration_data})
