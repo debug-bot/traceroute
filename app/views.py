18d21b301478aa00966e6488b3710575c191d4b7
Umair Ashraf <umairashraf5252@gmail.com>
2025-03-10 16:51:45 +0500

    fixed

--- app/views.py
+++ app/views.py
@@ -643,7 +643,7 @@ def configuration_view(request):
         )
     if requestType == "getConfigurations":
         device_id = request.GET.get("device_id")
-        configs = Configuration.objects.filter(router__id=device_id).all()
+        configs = Configuration.objects.filter(router__id=device_id).values_list('id','router.name','router.ip','version','created_at','file')
         return JsonResponse({"configs": configs})
 
     unique_cities = [
